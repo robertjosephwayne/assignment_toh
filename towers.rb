4b2de72af9cb988513cf37dd388f44f586ff8487
Robert Wayne <robertjwayne@gmail.com>
2017-02-03 00:06:01 -0500

    Add method to display instructions

--- towers.rb
+++ towers.rb
@@ -1,18 +1,17 @@
-
 class TowerOfHanoi
-    # Display instructions to user
-    # Set up game board
-      # Stack N disks on first rod with largest on bottom
-      # Rod two and three start out with no disks
-    # Render game board
-    # Until user quits or wins:
-      # Request move from user
-        # If input is 'quit', end the game
-        # If input is valid move
-          # Update game board
-          # Render game board
-          # If user won, output congratulations message
-        # If input is invalid, ask user to reenter
+  # Display instructions to user
+  # Set up game board
+  # Stack N disks on first rod with largest on bottom
+  # Rod two and three start out with no disks
+  # Render game board
+  # Until user quits or wins:
+  # Request move from user
+  # If input is 'quit', end the game
+  # If input is valid move
+  # Update game board
+  # Render game board
+  # If user won, output congratulations message
+  # If input is invalid, ask user to reenter
 
   def initialize(disks = 3)
     @disks = disks
@@ -22,7 +21,10 @@ class TowerOfHanoi
   end
 
   def instructions
-
+    puts "Welcome to Tower of Hanoi!"
+    puts "Instructions:"
+    puts "Enter where you'd like to move from and to"
+    puts "in the format '1,3'. Enter 'q' to quit."
   end
 
   def play
