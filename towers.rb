5e40daecb0d2cb7e8ae98d07acc9fac57879d5d5
Robert Wayne <robertjwayne@gmail.com>
2017-02-02 23:51:20 -0500

    Add initialize method

---
+++ towers.rb
@@ -0,0 +1,54 @@
+
+class TowerOfHanoi
+    # Display instructions to user
+    # Set up game board
+      # Stack N disks on first rod with largest on bottom
+      # Rod two and three start out with no disks
+    # Render game board
+    # Until user quits or wins:
+      # Request move from user
+        # If input is 'quit', end the game
+        # If input is valid move
+          # Update game board
+          # Render game board
+          # If user won, output congratulations message
+        # If input is invalid, ask user to reenter
+
+  def initialize(disks)
+    @disks = disks
+  end
+
+  def instructions
+
+  end
+
+  def new_game
+
+  end
+
+  def set_board
+
+  end
+
+  def render
+
+  end
+
+  def won?
+
+  end
+
+  def game_over
+
+  end
+
+  def valid_move?
+
+  end
+
+  def move_disk(a, b)
+
+  end
+
+end
+
